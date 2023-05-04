2e3e6a1ca1e8d296fed00e89c9f25eb717f33e5f
burakbaskayacmd <116830067+burakbaskayacmd@users.noreply.github.com>
2023-05-05 01:26:10 +0300

    update

--- submit.php
+++ submit.php
@@ -1,23 +1,37 @@
+
 <?php
-if (isset($_POST['age']) && isset($_POST['gender']) && isset($_POST['education']) && isset($_POST['job']) && isset($_POST['income']) && isset($_POST['submit'])
-) {
-  $age = $_POST['age'];
-  $gender = $_POST['gender'];
-  $education = $_POST['education'];
-  $job = $_POST['job'];
-  $income = $_POST['income'];
-  $submit = $_POST['submit'];
-  $who = $_POST['who'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $age = trim($_POST["age"]);
+  $gender = trim($_POST["gender"]);
+  $education = trim($_POST["education"]);
+  $job = trim($_POST["job"]);
+  $income = trim($_POST["income"]);
+
+  // Check that all form fields are filled in
+  if (empty($name) || empty($email) || empty($age) || empty($gender) || empty($education) || empty($job) || empty($income) || empty($message)) {
+    http_response_code(400);
+    echo "Please fill in all form fields.";
+    exit;
+  }
+
+  // Set up email
   $to = "burakbaskaya158@gmail.com";
-  $subject = "Form submission";
-  $body = "Age: $age\\nGender: $gender\\nEducation: $education\\nJob: $job\\nIncome: $income\\nWho: $who";
+  $subject = "New form submission";
+  $body = "Age: $age\n\nGender: $gender\n\nEducation: $education\n\nJob: $job\n\nIncome: $income";
 
   $headers = "From: $to";
 
+  // Send email
   if (mail($to, $subject, $body, $headers)) {
-    echo "Thank you for your message!";
+    http_response_code(200);
+    echo "Thank you! Your message has been sent.";
   } else {
-    echo "Something went wrong. Please try again later.";
+    http_response_code(500);
+    echo "Oops! Something went wrong and we couldn't send your message.";
   }
+
+} else {
+  http_response_code(403);
+  echo "There was a problem with your submission, please try again.";
 }
 ?>
