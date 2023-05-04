c8e5c4dc0a49205eddf618a177115a8397aef505
burakbaskayacmd <116830067+burakbaskayacmd@users.noreply.github.com>
2023-05-05 01:15:57 +0300

    update

--- submit.php
+++ submit.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-
 if (isset($_POST['age']) && isset($_POST['gender']) && isset($_POST['education']) && isset($_POST['job']) && isset($_POST['income']) && isset($_POST['submit'])
 ) {
   $age = $_POST['age'];
@@ -9,10 +7,10 @@ if (isset($_POST['age']) && isset($_POST['gender']) && isset($_POST['education']
   $job = $_POST['job'];
   $income = $_POST['income'];
   $submit = $_POST['submit'];
-
+  $who = $_POST['who'];
   $to = "burakbaskaya158@gmail.com";
   $subject = "Form submission";
-  $body = "Age: $age\\nGender: $gender\\nEducation: $education\\nJob: $job\\nIncome: $income";
+  $body = "Age: $age\\nGender: $gender\\nEducation: $education\\nJob: $job\\nIncome: $income\\nWho: $who";
 
   $headers = "From: $to";
 
